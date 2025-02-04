4ce1d8bbf681ffba422f15a3e0cb035ad4a661a0
alicemichel <alicejmichel@gmail.com>
2025-02-03 18:58:45 -0800

    another flip test

--- opensoundscape/audio.py
+++ opensoundscape/audio.py
@@ -1690,7 +1690,8 @@ def _audio_from_file_handler(
         file_duration = librosa.get_duration(path=path)
 
         #calculate true sampling rate, file internal relative to "real" GPS time
-        computed_sample_rate = file_duration * metadata['samplerate'] / gps_duration #n_samples/(end_seconds-beginning_seconds)
+        computed_sample_rate = file_duration * metadata['samplerate'] / gps_duration 
+        #n_samples/(end_seconds-beginning_seconds)
         # or librosa.get_samplerate(path=path)?
 
         #update metadata
@@ -1735,7 +1736,7 @@ def _audio_from_file_handler(
 
     if barlt == True:
         old_offset = offset
-        offset = old_offset * metadata['true_sample_rate'] / metadata['samplerate']
+        offset = old_offset * metadata['samplerate']/metadata['true_sample_rate'] ##TRYING FLIPPED HERE##
         print(f'updated time offset based on true sr from {old_offset} to {offset}')
     #true_sample_rate = n_samples / real_time
     #offset = offset * true_sample_rate / sample_rate
